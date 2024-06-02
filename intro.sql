349238286d9557c3c88904cea834ea2d64e63c9a
𝖲𝖨𝖥 𝖥𝖠𝖧𝖨𝖬 <siffahim25gmail.com>
2024-06-03 00:32:27 +0600

    feat: :sparkles: table operation and data type of postgressql

--- intro.sql
+++ intro.sql
@@ -1,8 +1,8 @@
--- Active: 1717176845480@@127.0.0.1@5432@university_management
+-- Active: 1717176845480@@127.0.0.1@5432@test2
 # database,Create,Rename,Delete
---CREATE DATABASE TEST2;
--- ALTER DATABASE test2 RENAME TO test3;
--- DROP DATABASE test1;
+CREATE DATABASE TEST2;
+ALTER DATABASE test2 RENAME TO test3;
+DROP DATABASE test1;
 
 # table create
 CREATE TABLE students(
@@ -13,7 +13,51 @@ CREATE TABLE students(
 )
 
 # Rename table
--- ALTER TABLE students RENAME TO learners;                         
+ALTER TABLE students RENAME TO learners;                         
 
 # Delete table
---DROP TABLE learners;
+DROP TABLE learners;
+
+# Constraints
+-- Multiple constrains use like this
+CREATE TABLE users(
+    user_Id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    age INT
+)
+
+# another way to give constraint
+CREATE TABLE users2(
+    user_Id SERIAL,
+    username VARCHAR(255)  NOT NULL,
+    email VARCHAR(255)  NOT NULL,
+    age INT,
+    PRIMARY KEY(user_id,username),
+    UNIQUE(username, email)
+)
+
+CREATE TABLE users(
+    user_Id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    age INT DEFAULT 18
+)
+
+#Insertion data to table
+INSERT INTO users VALUES(1,'abc','abc@gmail.com');
+
+#insert data defined with column name
+INSERT INTO users(username,email) VALUES('abc','abc@gmal.com');
+
+INSERT INTO users(username,email)
+VALUES
+('abc2','abc2@gmail.com'),
+('abc3','abc3@gmail.com')
+
+#table data delete without table
+TRUNCATE TABLE users;
+
+#Alter table
+
+SELECT * FROM users;
